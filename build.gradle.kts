63c246363c47f67c538557d1f22f6d1b262b928a
최범순 <138450966+depayse-paytalab@users.noreply.github.com>
2024-05-29 09:25:55 +0900

    feat : FastcampusIssueServiceRepo 초기화

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.5" apply false
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+}
+
+
+allprojects {
+    group = "com.fastcampus"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_18
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+        // JWT 인증
+        implementation("com.auth0:java-jwt:3.19.2")
+
+        // Kotlin Logging
+        implementation("io.github.microutils:kotlin-logging:1.12.5")
+
+        // Kotlin
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+        // H2DB
+        runtimeOnly("com.h2database:h2")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "18"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
